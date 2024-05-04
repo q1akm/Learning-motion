1400c9cf8ddc3d0f60cec659ecfa44e1b1b057c3
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-05-04 17:22:02 +0700

    some changes

--- src/routes/Basic.jsx
+++ src/routes/Basic.jsx
@@ -1,44 +1,25 @@
-import {useState } from "react"
-import gsap from 'gsap';
-import { SplitText } from "gsap/SplitText";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-gsap.registerPlugin(SplitText, ScrollTrigger);
-
-let mySplitText = new SplitText(".split",{type: "chars"} );
-let chars = mySplitText.chars;
-
-gsap.from(chars,{
-  yPercent: 130,
-  stagger: 0.05,
-  ease: "back.out",
-  duration: 1,
-  scrollTrigger: {
-    trigger: '.split',
-    start : 'top 20%',
-  }
-})
+import { useState } from "react"
+import { Link } from "react-router-dom";
 
 const Basic = () => {
-
-    const [counter, setCounter] = useState(0);
+  
+  const [count, setCount] = useState(100);
 
   return (
-   <>
-     <div className="flex flex-col text-3xl justify-center items-center">
-        <button onClick={()=> setCounter((prevCount) => prevCount -1)}>-</button>
-        <h1>{counter}</h1>
-        <button onClick={()=> setCounter((prevCount) => prevCount +1)}>+</button>
-    </div>
-
-    <div className=" border-2 w-full h-screen border-black">
-      <h1 className="split">Hello AKM</h1>
-      <h1>AKM</h1>
-    </div>
-    <div className="w-full h-screen">
-      <h1>hello</h1>
-    </div>
-   </>
+    <main className="w-screen h-screen flex justify-center items-center bg-black overflow-hidden">
+      
+      <div className="flex flex-col justify-center gap-5">
+        <button 
+        className="text-black text-lg bg-white rounded-xl"
+        onClick={() => setCount(count - 10)}>-</button>
+        <h1 className="text-white text-4xl text-bold">Count: {count}</h1>
+        <button 
+        className="text-black text-lg bg-white rounded-xl"
+        onClick={() => setCount(count + 10)}>+</button>
+        <Link to="/" className="text-white ">back/home</Link>
+      </div>
+      
+    </main>
   )
 }
 
