3d9d919be6fbae78c2b2f6e762ef1926bb36f683
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-12-09 11:44:57 +0700

    added svg scroll animation

--- src/routes/GSAP001.jsx
+++ src/routes/GSAP001.jsx
@@ -1,8 +1,9 @@
 import gsap from 'gsap';
 import React, { useEffect, useRef, useState } from 'react'
-import { ScrollTrigger } from 'gsap/all';
+import { DrawSVGPlugin, ScrollTrigger } from 'gsap/all';
 
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(DrawSVGPlugin);
 
 function GSAP001 (){
 
@@ -10,6 +11,7 @@ function GSAP001 (){
   const boxRef2 = useRef(null);
   const boxRef3 = useRef(null);
   const boxRef4 = useRef(null);
+  const pathRef = useRef(null);
 
   useEffect(() => {
    const tl = gsap.timeline();
@@ -25,13 +27,26 @@ function GSAP001 (){
         start: "top 30%",
         end: "top -10%",
         scrub: true,
-        markers: true,
         pin: true
       }
     });
 
     tl2. to(boxRef3.current, {  x: 200, y: 100, duration: 2,})
     tl2. to(boxRef4.current, {  x: 400, y: 100, duration: 1, background: "red"})
+
+    gsap.fromTo(
+      pathRef.current, 
+      {drawSVG: "0%"},
+      {drawSVG: "100%", duration: 2,
+        scrollTrigger: {
+          trigger: pathRef.current,
+          start: "top 90%",
+          end: "top 50%",
+          scrub: true,
+        }
+      }
+    );
+
   }, [])
 
   const [color, setColor] = useState(false);
@@ -64,19 +79,29 @@ function GSAP001 (){
         {color ? "blue" : "red"}
       </button>
     </div>
-   <div className='border-2 border-black'>
-    <div className='w-full h-[50vh]'/>
-    <div 
-    ref={boxRef3}
-    className='w-[100px] h-[100px] bg-red-700'></div>
-    <div 
-    ref={boxRef4}
-    className='w-[100px] h-[100px] bg-indigo-500'></div>
-    <div className='w-full h-[100vh]'/>
-    
-   </div>
-   <br />
-   <br />
+    <div className='border-2 border-black'>
+      <div className='w-full h-[50vh]'/>
+      <div 
+      ref={boxRef3}
+      className='w-[100px] h-[100px] bg-red-700'></div>
+      <div 
+      ref={boxRef4}
+      className='w-[100px] h-[100px] bg-indigo-500'></div>
+      <div className='w-full h-[100vh]'/>
+      
+    </div>
+    <div className='border-2 border-red-500'>
+      <div className='w-full h-[50vh]'/>
+      <svg width={400} height={500}>
+        <path
+        ref={pathRef}
+        stroke='blue'
+        d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+        fill='transparent'
+        strokeWidth="2"/>
+      </svg>
+      <div className='w-full h-[50vh]'/>
+    </div>
    </main>
   )
 }
